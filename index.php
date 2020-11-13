0e88b0bf65d35d55dff64555e542029ab48e637d
Oyishoma <69061611+Oyishoma@users.noreply.github.com>
2020-11-13 08:17:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
   <!-- Web fonts-->
   <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i&amp;display=swap" rel="stylesheet">
   <!-- CSS vendors-->
-  <link rel="stylesheet" href="css/bootstrap-custom.css">
   <link rel="stylesheet" type="text/css" href="css/bootstrap-custom.css">
   <link rel="stylesheet" type="text/css" href="css/ionicons.min.css">
   <link rel="stylesheet" type="text/css" href="css/tiny-slider.css">
